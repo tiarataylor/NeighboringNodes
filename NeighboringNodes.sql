8812f257cb9c59f0053cc47fcd3d0974c171bed1
tiarataylor <17831049+tiarataylor@users.noreply.github.com>
2018-12-24 15:00:37 -0500

    Update NeighboringNodes.sql

--- NeighboringNodes.sql
+++ NeighboringNodes.sql
@@ -1,6 +1,2 @@
 #mysql
 CREATE TABLE NeighboringNodes (size INT,i INT, x INT, y INT, INDEX(size), PRIMARY KEY(size,i));
-SELECT * FROM NeighboringNodes;
-SELECT * FROM NeighboringNodes where size = 3;
-#querying neighbor nodes and specified node with index 5 on grid of size 3
-SELECT * FROM NeighboringNodes where size = 3 and ((x = 1 and y <= 2 and y >=0) or (x >= 0 and x <= 2 and y = 1));
